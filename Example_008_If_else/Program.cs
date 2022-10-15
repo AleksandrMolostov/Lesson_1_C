84e09067e82605598c74722ee5e6f552ca456c37
Aleksandr <alexandr.igig@gmail.com>
2022-10-15 15:36:17 +0300

    Add Example_007

---
+++ Example_008_If_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+   Console.WriteLine("Cool!"); 
+}
+else
+{
+    Console.WriteLine("Hello");
+    Console.WriteLine(username);
+}
